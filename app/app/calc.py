1c4a750d9dd867ee480ae290495693f39345ae5b
Pramish K.C <pramishkc066@gmail.com>
2025-10-11 16:53:46 +0545

    DB Configuration

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator module
 """
 
+
 def add(a, b):
     """Return the sum of a and b."""
     return a + b
+
 
 def substract(a, b):
     """Subtract b from a and return the result."""
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,12 +1,13 @@
 """
 Sample tests
 """
-
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
+
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
